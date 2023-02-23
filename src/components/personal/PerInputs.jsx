df16babdf2d5a13c4442cd43f7ae4c4c6d46715d
bladwing <timoti.ponarenko@gmail.com>
2023-02-23 16:51:43 +0400

    create inputs and general style for them

--- src/components/personal/PerInputs.jsx
+++ src/components/personal/PerInputs.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Inputs = () => {
-    return (
-        <div className='First-Side'>
+  return (
+    <div className="First-Side">
+      <form action="">
+        <table>
+          <thead>
+            <tr className="Short-inputs-Container">
+              <td className="Short-inputs-field">
+                <label htmlFor="">Name</label>
+                <input type="text" name="" id="" placeholder="John"/>
+                <span>Minimum 2 symbol</span>
+              </td>
+              <td className="Short-inputs-field">
+                <label htmlFor="">LastName</label>
+                <input type="text" name="" id="" placeholder="Manson"/>
+                <span>Minimum 2 symbol</span>
+              </td>
+            </tr>
+            <tr>
+              <td className="Upload-file">
+                <input type="file" name="" id="" />
+              </td>
+            </tr>
+            <tr >
+              <td className="Desctiption">
+                <label htmlFor="">Description</label>
+                <textarea type="text" name="" id="" />
+              </td>
+            </tr>
+            <tr>
+              <td className="Long-inputs-field">
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                
+                />
+                <span>minimum 2 symbol</span>
+              </td>
+              </tr>
+              <tr >
+              <td className="Long-inputs-field">
+                <label htmlFor="">Phone</label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                />
+                <span>minimum 2 symbol</span>
+              </td>
+
             
-        </div>
-    );
+      
+            </tr>
+            <tr>
+              <td></td>
+            </tr>
+          </thead>
+        </table>
+      </form>
+      <Link to="/expirience">Next</Link>
+    </div>
+  );
 };
 
 export default Inputs;
