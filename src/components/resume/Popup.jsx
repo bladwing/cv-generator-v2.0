d67760451dcb7289a7c9311eb66f60ac904516ff
bladwing <timoti.ponarenko@gmail.com>
2023-03-02 20:06:21 +0400

    fix style for popup

--- src/components/resume/Popup.jsx
+++ src/components/resume/Popup.jsx
@@ -6,11 +6,11 @@ const Popup = () => {
 
   return (
     <div
-      className="Resume-Popup"
+      className="Resume-Popup-Container"
       style={{ display: show === "true" ? "block" : "none" }}
     >
       <button onClick={() => setShow(false)}>x</button>
-      <h2>Resume is ready</h2>
+      <h2>Resume is ready 🎉</h2>
     </div>
   );
 };
